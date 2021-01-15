4b11d1db21b55f3bcb9a598c7d9e70e9a9862299
vwu1711 <43825288+vwu1711@users.noreply.github.com>
2021-01-14 17:54:47 -0800

    Add files via upload

--- bluetoothReceiveSerial.ino
+++ bluetoothReceiveSerial.ino
@@ -5,9 +5,6 @@
 SoftwareSerial mySerial(0,1); //rx | tx
 #define ledPin 15
 int state = 0;
-unsigned char buttonState;
-unsigned char i = 0;
-char arr[] = {};
 
 void setup() {
   mySerial.begin(38400);
@@ -17,7 +14,6 @@ void setup() {
 void loop() {
   if(mySerial.available()>0){
     state = mySerial.read();
-    //Serial.println(state);
   }
 
   if (state == '1'){
@@ -30,28 +26,5 @@ void loop() {
     Serial.println('0');
     state = 0;
   }
-
- /* buttonState = mySerial.available();
-
-    if (mySerial.available() > 0){
-      while(buttonState > 0){  // count chars from buffer
-          arr[i] = mySerial.read() ;
-          Serial.print(arr[i]) ;  // debug statement, print to serial
-          //mySerial.print(arr[i]) ;      // debug statement, print to BT terminal
-
-          if (arr[i] == '1'){
-            digitalWrite(ledPin, HIGH);
-            state = 0;
-          }
-          else if(arr[i] == '0'){
-            digitalWrite(ledPin, LOW);
-            state = 0;
-          }
-
-          i++ ;
-          buttonState-- ;
-      }
-    }*/
-    
     
 }
