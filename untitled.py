02b81bebc40d330db51328facbdd709c8229567d
cwu051 <38019809+cwu051@users.noreply.github.com>
2021-01-30 14:51:05 -0800

    Add files via upload

--- untitled.py
+++ untitled.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  distance_to_camera.py
+#  untitled.py
 #  
 #  Copyright 2021  <pi@raspberrypi>
 #  
@@ -22,18 +22,15 @@
 #  
 #  
 
-# import the necessary packages
+
 import numpy as np
-import imutils
 import cv2
+
 def find_marker(image):
 	# convert the image to grayscale, blur it, and detect edges
-	color = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	lowred = np.array([161, 155, 84])
-	highred = np.array([179, 255, 255])
-	redmask = cv2.inRange(color, lowred, highred)
-	color = cv2.GaussianBlur(redmask, (5, 5), 0)
-	edged = cv2.Canny(redmask, 100, 200)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 35, 125)
 	# find the contours in the edged image and keep the largest one;
 	# we'll assume that this is our piece of paper in the image
 	cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -42,10 +39,6 @@ def find_marker(image):
 	# compute the bounding box of the of the paper region and return it
 	return cv2.minAreaRect(c)
     
-def distance_to_camera(knownWidth, focalLength, perWidth):
-	# compute and return the distance from the maker to the camera
-	return (knownWidth * focalLength) / perWidth
-
 cap = cv2.VideoCapture(0)
 # initialize the known distance from the camera to the object, which
 # in this case is 24 inches
@@ -56,27 +49,7 @@ KNOWN_WIDTH = 11.0
 # load the furst image that contains an object that is KNOWN TO BE 2 feet
 # from our camera, then find the paper marker in the image, and initialize
 # the focal length
-#ret, image = cap.read()
-#marker = find_marker(image)
-focalLength = 543.45
-#(marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
-
-# loop over the images
-while(True):
-	# load the image, find the marker in the image, then compute the
-	# distance to the marker from the camera
-	ret, image = cap.read()
-	marker = find_marker(image)
-	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-	# draw a bounding box around the image and display it
-	box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-	box = np.int0(box)
-	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-	cv2.putText(image, "%.2fft" % (inches / 12),
-		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-		2.0, (0, 255, 0), 3)
-	cv2.imshow("image", image)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-cap.release()
-cv2.destroyAllWindows()
+ret, image = cap.read()
+marker = find_marker(image)
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+print(focalLength)
