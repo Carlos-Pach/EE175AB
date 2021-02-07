48e5bf3806c233db1aefff1c165e3c7c5003d080
cwu051 <38019809+cwu051@users.noreply.github.com>
2021-02-07 12:44:37 -0800

    Add files via upload

--- distance_to_camera.py
+++ distance_to_camera.py
@@ -26,25 +26,30 @@
 import numpy as np
 import imutils
 import cv2
+        
 def find_marker(image):
-	# convert the image to grayscale, blur it, and detect edges
-	color = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	lowred = np.array([161, 155, 84])
-	highred = np.array([179, 255, 255])
-	redmask = cv2.inRange(color, lowred, highred)
-	#color = cv2.GaussianBlur(redmask, (5, 5), 0)
-	#edged = cv2.Canny(redmask, 100, 200)
-	# find the contours in the edged image and keep the largest one;
-	# we'll assume that this is our piece of paper in the image
-	cnts = cv2.findContours(redmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	c = max(cnts, key = cv2.contourArea)
-	# compute the bounding box of the of the paper region and return it
-	return cv2.minAreaRect(c)
+        # convert the image to grayscale, blur it, and detect edges
+        color = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lowred = np.array([161, 155, 84])
+        highred = np.array([179, 255, 255])
+        redmask = cv2.inRange(color, lowred, highred)
+        #color = cv2.GaussianBlur(redmask, (5, 5), 0)
+        #edged = cv2.Canny(redmask, 100, 200)
+        # find the contours in the edged image and keep the largest one;
+        # we'll assume that this is our piece of paper in the image
+        cnts = cv2.findContours(redmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        if len(cnts) > 0:
+                c = max(cnts, key = cv2.contourArea)
+                return cv2.minAreaRect(c)
+        else:
+        # compute the bounding box of the of the paper region and return it
+                return 0
     
 def distance_to_camera(knownWidth, focalLength, perWidth):
-	# compute and return the distance from the maker to the camera
-	return (knownWidth * focalLength) / perWidth
+        # compute and return the distance from the maker to the camera
+        return (knownWidth * focalLength) / perWidth
+
 
 cap = cv2.VideoCapture(0)
 # initialize the known distance from the camera to the object, which
@@ -61,22 +66,24 @@ KNOWN_WIDTH = 11.0
 focalLength = 543.45
 #(marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
+
 # loop over the images
 while(True):
-	# load the image, find the marker in the image, then compute the
-	# distance to the marker from the camera
-	ret, image = cap.read()
-	marker = find_marker(image)
-	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-	# draw a bounding box around the image and display it
-	box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-	box = np.int0(box)
-	cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-	cv2.putText(image, "%.2fft" % (inches / 12),
-		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-		2.0, (0, 255, 0), 3)
-	cv2.imshow("image", image)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+        # load the image, find the marker in the image, then compute the
+        # distance to the marker from the camera
+    ret, image = cap.read()
+    marker = find_marker(image)
+    if marker == 0:
+        cv2.putText(image,'Target Lost',(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_4)
+    else:
+        inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+        # draw a bounding box around the image and display it
+        box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+        cv2.putText(image, "%.2fft" % (inches / 12),(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+    cv2.imshow("image", image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 cap.release()
 cv2.destroyAllWindows()
