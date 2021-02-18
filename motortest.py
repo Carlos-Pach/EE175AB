bc656c69f6cdf7d6e46278455c78c43831e1ad16
cwu051 <38019809+cwu051@users.noreply.github.com>
2021-02-18 13:07:58 -0800

    Add files via upload

---
+++ motortest.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  motortest.py
+#  
+#  Copyright 2021  <pi@raspberrypi>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+from smbus import SMBus
+
+addr = 0x8
+bus = SMBus(1)
+
+numb = 1
+
+print("Enter 1 for ON or 0 for OFF")
+while numb == 1:
+    
+    ledstate = input(">>>>  ")
+    
+    if ledstate == "1": #Haven't changed yet, but this should output 32 in binary.
+        change = 45
+        bus.write_byte(addr, change)
+    elif ledstate == "0": #Output 0 in binary
+        change = 80
+        bus.write_byte(addr, change)
+    else:
+        numb = 0
