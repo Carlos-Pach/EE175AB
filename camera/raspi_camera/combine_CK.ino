027a8a9e4e53b3e60a40d20aa70e3afb6b963fe1
taifuto <kduon013@ucr.edu>
2021-03-02 19:18:19 -0800

    updated soln to camera .py/.ino scripts

---
+++ camera/raspi_camera/combine_CK.ino
@@ -0,0 +1,102 @@
+/*
+RECEIVE DATA FROM PI: obj_num_g, degrees_g, c_dist_g (call in outside function ex. printPi)
+*/
+//https://dronebotworkshop.com/i2c-arduino-raspberry-pi/
+// https://roboticsbackend.com/raspberry-pi-master-arduino-slave-i2c-communication-with-wiringpi/
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x08
+
+//Kelly: Serial.print for debugging, take out later.
+//Final variables: 
+//1. obj_num_g , 2. degrees_g , 3. c_dist_g;
+//1. object detected number 2. degree turn for water sprayer (0-180) 3. Distance from largest red object
+volatile unsigned int obj_num_g=0;
+volatile unsigned int degrees_g=0;
+volatile float c_dist_g=0; 
+
+volatile unsigned char buf[8] = "";     //recieves pi info
+volatile unsigned int arr[4]; // ={0};   //convert buf array to int() IMP! TO ASSIGN TO EMPTY ARRAY OR COULD GET RANDOM NUMBERS FOR DIFF INDEXES
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+ // Wire.onRequest(sendData);  //if needed to send data to pi
+}
+
+void loop() {  delay(1000); }  //250
+ 
+ void receiveData(volatile int byteCount) { 
+   while(Wire.available()) {  //Wire.available() returns the number of bytes available for retrieval with Wire.read(). Or it returns TRUE for values >0.
+      for(volatile int i=0; i<byteCount; i++){
+         buf[i] = Wire.read();
+         //Serial.print(buf[2], DEC);
+        arr[i]=int(buf[i]);
+       }
+      
+      //if if 1 in the first 4 bits of ex. 1000, then run script, else skip.
+      if ((arr[0] & 8)==8 ){
+        volatile unsigned int lsb=0;
+        //2. Kelly's Degrees for water sprayer to turn (0-180)
+          degrees_g=0;
+          degrees_g= 0x00 | arr[1]<<4;
+          if (arr[2]>50){ //just in case of outlier #s, optional
+             lsb = 1; }
+          else { lsb = arr[2]; }
+          degrees_g= degrees_g | lsb;          
+        
+    //1. Kelly's Object Number
+    //on first 4 bits, bit mask last 3 bits, so ex. 0111, each number 0-7 corresponds to a hard-coded char string array name: ex. raccoon 
+        obj_num_g= arr[0] & 7; 
+          // if (obj_num_g==0) {
+          //   strcpy(name, "zero");}
+          // else if (obj_num_g==1){
+          //   strcpy(name, "one");}
+          // else if (obj_num_g==2){
+          //   strcpy(name, "two");}
+          // else if (obj_num_g==3){
+          //   strcpy(name, "three");}
+          // else if (obj_num_g==4){
+          //   strcpy(name, "squirrel");}
+          // else if (obj_num_g==5){
+          //   strcpy(name, "raccoon");}
+          // else if (obj_num_g==6){
+          //   strcpy(name, "person");}
+          // else if (obj_num_g==7){
+          //   strcpy(name, "potted plant");}
+          // else { } 
+      }
+  else{  //Serial.print("nothing! Does not detect one of 8 objects, COMMENT OUT\n\n")  ;
+      degrees_g, obj_num_g = 0;  }  
+      
+      //3. Chad's Distance from red object inches, so convert to cm first
+      c_dist_g=0;
+      c_dist_g = float(arr[3]*(2.54));
+    
+// @CARLOS: Refer obj_num_g to corresp. name of object commented up above 
+  printPi(degrees_g, c_dist_g, obj_num_g);
+  Serial.print("\n");
+  } 
+}
+
+
+void printPi( volatile unsigned int& degr, volatile float& c_di, volatile unsigned int& obj_num_g)
+{
+  Serial.print("detected obj: ");
+  Serial.print(obj_num_g);
+  Serial.print("\n degrees_g to turn water sprayer: ");
+  Serial.print(degr);
+  Serial.print("\n Object Avoidence, aka distance from red obj: ");
+  Serial.print(c_di);
+  Serial.print("\n\n");
+}
+
+
+void sendData()
+{
+  //uneeded until we use to send data to pi?
+  //Wire.write(data_to_echo);
+  //Serial.print("send data to pi \n\n");
+  }
+
--- camera/raspi_camera/combine_CK.py
+++ camera/raspi_camera/combine_CK.py
@@ -14,50 +14,50 @@ block=[]            # orig data read by TF; added by .append()
 addr = 0x8          #for i2c bus address
 bus = SMBus(1)      #for i2c using ic2-1 rather than ic2-0 port
 c=0                 #for mapping coordinate of obj
-data_bet= [0,0,0]   #init final data [] to send over
+data_bet= [0,0,0]   #zeros init final data [] to send over
 flag=0              #see if there is a detection or not
 
 
 
 
-def send_to_pi(data)
-    #data=[c,potted plant] #EXAMPLE INPUT DATA
+def send_to_pi(data):
+    #data=[c,"potted plant"] #EXAMPLE INPUT DATA
     #take 3 pieces of data, send to pi via i2c
-    #1st bit if data is available
-    #2-4th bit name of detected obj
-    #5-12th bit coord of obj in degrees for sprayer    
+    #1st bit: if data is available
+    #2-4th bit: name of detected obj
+    #5-12th bit: coord of obj in degrees for sprayer    
 
     data_arr = ['0000','0000','0000'] #init byte array, size 3
-    if (data[0] )==0
+    if (data[0] )==0:
         return [0,0,0]      #tells Teensy not to read empty data
-    else
+    else:
         data_arr[0] =(format(0x8,'04b')) #puts a 1 on left-most bit to show info is coming 
-                                         #(optional bin to see array in print funct)
+                                         #(optional: bin to see array in print funct)
 
         #map obj names to a string of 4bit binary for ea of 8 obj classes ex.(0000 or 0001)
-        obj= (data[1][2]) 
-        if obj==ze
+        obj= (data[1][:2]) 
+        if obj=="ze":
             obj=(format(0x00,'04b'))
-        elif obj==on
+        elif obj=="on":
             obj=(format(0x01,'04b'))
-        elif obj==tw
+        elif obj=="tw":
             obj=(format(0x02,'04b'))
-        elif obj==th
+        elif obj=="th":
             obj=(format(0x03,'04b'))
-        elif obj==sq
+        elif obj=="sq":
             obj=(format(0x04,'04b'))
-        elif obj==ra
+        elif obj=="ra":
             obj=(format(0x05,'04b'))
-        elif obj==pe
+        elif obj=="pe":
             obj=(format(0x06,'04b'))
-        elif obj==po
+        elif obj=="po":
             obj=(format(0x07,'04b'))
-        else
-            print(Madam, something's wrong, this isn't an avaliable object.)
+        else:
+            print("Madam, something's wrong, this isn't an avaliable object.")
 
-        #first 4 bits fstring (faster than format) to complete binary string 
-        #combine bits to create (1st for availablility, last 3 for object name)
-        data_arr[0] = f{(int(data_arr[0],2)  int(obj,2))b} #aka f{insert int(string bit  object bit)b} aka format(c, b)
+        #first 4 bits: fstring (faster than format) to complete binary string 
+        #combine bits: to create (1st for availablility, last 3 for object name)
+        data_arr[0] = f"{(int(data_arr[0],2) | int(obj,2)):b}" #aka f"{insert int(string bit | object bit):b}" aka format(c, "b")
         
         #for obj coord. create bin string size 8bits, this goes last in final bits
         coord=data[0]
@@ -67,20 +67,20 @@ def send_to_pi(data)
         it=1                #iteration number, happens 2x for the 1 byte coordinates takes up
         c=0                 #counts for 4 bits per set of bits (2 sets for 1 byte)
         temp_byt=''
-        for v in bin_coord #iterable string ! D
+        for v in bin_coord: #iterable string ! :D
                             #for the first 4 take it, then next 4 
                             #save and add to bin string until reached 3rd set then export as a byte to datat_array
             temp_byt=temp_byt+v
             c=c+1
-            if c==4
+            if c==4:
                 c=0
                 data_arr[it]= temp_byt
                 it=it+1
                 temp_byt=''                
 
         #convert to decimal form to send to pi
-        for byttte in data_arr 
-            data_bet[c]= (int(byttte,2))  #optionally for hex string, wrap hex outside of int
+        for byttte in data_arr: 
+            data_bet[c]= (int(byttte,2))  #optionally: for hex string, wrap hex outside of int
             c=c+1
     return data_bet
 
@@ -91,15 +91,15 @@ def send_to_pi(data)
 
 
 # Define VideoStream class to handle streaming of video from webcam in separate processing thread
-# Source - Adrian Rosebrock, PyImageSearch httpswww.pyimagesearch.com20151228increasing-raspberry-pi-fps-with-python-and-opencv
-class VideoStream
-    Camera object that controls video streaming from the Picamera
-    def __init__(self,resolution=(640,480),framerate=30)
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self,resolution=(640,480),framerate=30):
     #is the lowest supported resolution already!
 
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('MJPG'))
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
             
@@ -109,16 +109,16 @@ class VideoStream
     # Variable to control when the camera is stopped
         self.stopped = False
 
-    def start(self)
+    def start(self):
     # Start the thread that reads frames from the video stream
         Thread(target=self.update,args=()).start()
         return self
 
-    def update(self)
+    def update(self):
         # Keep looping indefinitely until the thread is stopped
-        while True
+        while True:
             # If the camera is stopped, stop the thread
-            if self.stopped
+            if self.stopped:
                 # Close camera resources
                 self.stream.release()
                 return
@@ -126,15 +126,15 @@ class VideoStream
             # Otherwise, grab the next frame from the stream
             (self.grabbed, self.frame) = self.stream.read()
 
-    def read(self)
+    def read(self):
     # Return the most recent frame
         return self.frame
 
-    def stop(self)
+    def stop(self):
     # Indicate that the camera and thread should be stopped
         self.stopped = True
         
-def find_marker(image)
+def find_marker(image):
         # convert the image to grayscale, blur it, and detect edges
         color = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         lowred = np.array([161, 155, 84])
@@ -146,19 +146,19 @@ def find_marker(image)
         # we'll assume that this is our piece of paper in the image
         cnts = cv2.findContours(redmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-        if len(cnts)  0
+        if len(cnts) > 0:
                 c = max(cnts, key = cv2.contourArea)
                 return cv2.minAreaRect(c)
-        else
+        else:
         # compute the bounding box of the of the paper region and return it
                 return 0
     
-def distance_to_camera(knownWidth, focalLength, perWidth)
+def distance_to_camera(knownWidth, focalLength, perWidth):
         # compute and return the distance from the maker to the camera
-        if perWidth == 0
+        if perWidth == 0:
             return 0
-        else
-            return (knownWidth  focalLength)  perWidth
+        else:
+            return (knownWidth * focalLength) / perWidth
 
 # Define and parse input arguments
 parser = argparse.ArgumentParser()
@@ -185,9 +185,9 @@ imW, imH = int(resW), int(resH)
 # Import TensorFlow libraries
 # If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
 pkg = importlib.util.find_spec('tflite_runtime')
-if pkg
+if pkg:
     from tflite_runtime.interpreter import Interpreter
-else
+else:
     from tensorflow.lite.python.interpreter import Interpreter
 
 # Get path to current working directory
@@ -200,13 +200,13 @@ PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
 PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
 
 # Load the label map
-with open(PATH_TO_LABELS, 'r') as f
+with open(PATH_TO_LABELS, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
-# Have to do a weird fix for label map if using the COCO starter model from
-# httpswww.tensorflow.orglitemodelsobject_detectionoverview
-# First label is '', which has to be removed.
-if labels[0] == ''
+# Have to do a weird fix for label map if using the COCO "starter model" from
+# https://www.tensorflow.org/lite/models/object_detection/overview
+# First label is '???', which has to be removed.
+if labels[0] == '???':
     del(labels[0])
 
 # Load the Tensorflow Lite model.
@@ -245,12 +245,12 @@ KNOWN_WIDTH = 2.547
 #ret, image = cap.read()
 #marker = find_marker(image)
 focalLength = 1170.6
-#(marker[1][0]  KNOWN_DISTANCE)  KNOWN_WIDTH
+#(marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 newim = 0
 pastim = 0
 
-#for frame1 in camera.capture_continuous(rawCapture, format=bgr,use_video_port=True)
-while True
+#for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+while True:
 
     # Start timer (for calculating frame rate)
     t1 = cv2.getTickCount()
@@ -265,8 +265,8 @@ while True
     input_data = np.expand_dims(frame_resized, axis=0)
 
     # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model
-        input_data = (np.float32(input_data) - input_mean)  input_std
+    if floating_model:
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
     # Perform the actual detection by running the model with the image as input
     interpreter.set_tensor(input_details[0]['index'],input_data)
@@ -279,31 +279,31 @@ while True
     #num = interpreter.get_tensor(output_details[3]['index'])[0]  # Total number of detected objects (inaccurate and not needed)
     # Loop over all detections and draw detection box if confidence is above minimum threshold
     flag=0   #init flag for sending to i2c
-    for i in range(len(scores))
+    for i in range(len(scores)):
         cv2.line(frame, (xmid, 0),(xmid,900), (255,255,0),5 ) #this makes sure the line stays after image has disappeared
         
-        if ((scores[i]  min_conf_threshold) and (scores[i] = 1.0))
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
             flag=1 #there is obj detected
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0]  imH)))
-            xmin = int(max(1,(boxes[i][1]  imW)))
-            ymax = int(min(imH,(boxes[i][2]  imH)))
-            xmax = int(min(imW,(boxes[i][3]  imW)))
-            xmid = int((xmin+xmax) 2)
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            xmid = int((xmin+xmax) /2)
             # MAP Xmid Coordinate TO DEGREES [for water sprayer to spray (by using interpolation)]
             c=int(np.interp(xmid, (0, 640), (0, 180)) )
-            print(xmid, Xmiddle)
-            print(c, convert to degrees)
+            #print(xmid, "Xmiddle")
+            #print(c, "convert to degrees")
             block.clear()
             block.append(c)
-            cv2.line(frame, (xmid, 0),(xmid,900), (255,255,0),5 ) #draw the line down center
+            cv2.line(frame, (xmid, 0),(xmid,900), (255,255,0),5 ) #draw the blue line down center of detected obj.
 
             cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
             # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from labels array using class index
-            label = '%s %d%%' % (object_name, int(scores[i]100)) # Example 'person 72%'
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
             labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
             label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
             cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
@@ -313,40 +313,41 @@ while True
 
 
     # Draw framerate in corner of frame
-    cv2.putText(frame,'FPS {0.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
-    cv2.putText(frame,'FPS for detection {0.2f}'.format(frame_rate_calc2),(30,75),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame,'FPS for detection: {0:.2f}'.format(frame_rate_calc2),(30,75),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
     #convert, then send data over w i2c
-    if (flag==0)
-        block=[0,placeholder] #set flag for leftmost bits to 0, don't do more math
+    if (flag==0):
+        block=[0,"placeholder"] #set flag for leftmost msb bits to 0, don't do more math
     dec_list = send_to_pi(block)
     
     marker = find_marker(frame)
-    if marker == 0
+    if marker == 0:
         inches = 0
-        cv2.putText(frame, %.2fft % (0  12),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
-    else
+        cv2.putText(frame, "%.2fft" % (0 / 12),(frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+    else:
         inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
         # draw a bounding box around the image and display it
-        newim = int(inches  2.54)
-        if pastim == 0
+        newim = int(inches * 2.54)
+        if pastim == 0:
             pastim = newim
-        else
-            if abs(pastim - newim)  5
+        else:
+            if abs(pastim - newim) < 5:
                 pastim = newim
-            else
+            else:
                 pastim = pastim
         box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
         box = np.int0(box)
         cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
-        cv2.putText(frame, %.2fcm % (pastim),(frame.shape[1] - 300, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+        cv2.putText(frame, "%.2fin" % (pastim),(frame.shape[1] - 300, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
     
-    if pastim=256
+    if pastim>=256 or marker==0:
 	    pastim=0
     dec_list.append(pastim)
-    print(dec_list, DEC_LIST SHOULD ALSO HAVE CHAD'S DIST)
+    print(dec_list, "DEC_LIST SHOULD ALSO HAVE CHAD'S DIST in list[3] \n")
     msg = i2c_msg.write(addr,dec_list)
     bus.i2c_rdwr(msg)
+    time.sleep(.3)
     dec_list.pop(3)
     
     
@@ -355,16 +356,15 @@ while True
 
     # Calculate framerate
     t2 = cv2.getTickCount()
-    time1 = (t2-t1)freq
-    frame_rate_calc= 1time1
+    time1 = (t2-t1)/freq
+    frame_rate_calc= 1/time1
 
     # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q')
-        print('FPS {0.2f}'.format(frame_rate_calc))
+    if cv2.waitKey(1) == ord('q'):
+        print('FPS: {0:.2f}'.format(frame_rate_calc))
         break
 
-#data_to_t = bus.write_block_data(addr,(block))
-print(block,  BLOCK LIST )
+print(block, " BLOCK LIST ")
 # Clean up
 cv2.destroyAllWindows()
 videostream.stop()
